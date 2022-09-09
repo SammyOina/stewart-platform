148d75d36e7b59021ef789cbcbac9ab5f9d17f42
sammy <sammyoina@gmail.com>
2022-09-09 18:55:07 +0300

    f(firmware) add websockets and change pb

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "WiFi.h"
-#include <ArduinoJson.h>
-#include <test.pb.h>
+#include <WebSocketsClient.h>
+#include <events.pb.h>
 #include <pb.h>
 #include <pb_common.h>
 #include <pb_decode.h>
@@ -10,11 +10,46 @@
 const char *ssid = "sammy2";
 const char *password = "12345678";
 const char *addr = "192.168.137.1";
-const uint16_t port = 10101;
+const uint16_t port = 8080;
 
-WiFiClient client;
+WebSocketsClient webSocket;
 
 uint8_t buffer[128];
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+	switch(type) {
+		case WStype_DISCONNECTED:
+			Serial.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED:
+			Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+			// send message to server when Connected
+			//webSocket.sendTXT("Connected");
+			//webSocket.sendTXT("hey");
+			break;
+		case WStype_TEXT:
+			Serial.printf("[WSc] get text: %s\n", payload);
+
+			// send message to server
+			// webSocket.sendTXT("message here");
+			break;
+		case WStype_BIN:
+			Serial.printf("[WSc] get binary length: %u\n", length);
+
+			// send data to server
+			// webSocket.sendBIN(payload, length);
+			break;
+		case WStype_ERROR:			
+		case WStype_FRAGMENT_TEXT_START:
+		case WStype_FRAGMENT_BIN_START:
+		case WStype_FRAGMENT:
+		case WStype_FRAGMENT_FIN:
+			break;
+	}
+
+}
 
 void setup()
 {
@@ -30,42 +65,36 @@ void setup()
         Serial.println("WIFI connection failed, reconnecting...");
         delay(500);
     }
+    
+    	// server address, port and URL
+	webSocket.begin("192.168.137.1", port, "/imu", "");
 
-    DynamicJsonDocument doc(1024);
+	// event handler
+	webSocket.onEvent(webSocketEvent);
 
-    doc["leg_strain"][0] = 1.302038;
-    doc["leg_strain"][1] = 2.302038;
-    doc["leg_strain"][2] = 3.302038;
-    doc["leg_strain"][3] = 4.302038;
-    doc["leg_strain"][4] = 5.302038;
-    doc["leg_strain"][5] = 6.302038;
-
-    serializeJson(doc, buffer);
-
-    TestMessage test_m = TestMessage_init_zero;
-    test_m.test_number = 12;
+	// try ever 5000 again if connection has failed
+	webSocket.setReconnectInterval(5000);
 }
 
 void loop()
 {
-    TestMessage test_m = TestMessage_init_zero;
-    test_m.test_number = 12;
-    if (!client.connect(addr, port))
-    {
-        Serial.println("connection failed");
-        Serial.println("wait 5 sec to reconnect...");
-        delay(5000);
-        return;
-    }
+	SensorEvent test = SensorEvent_init_zero;
+	test.event.iMUEvent.pitch = 1.1;
+	test.event.iMUEvent.roll = 2.2;
+	test.event.iMUEvent.yaw = 3.3;
+	test.which_event = SensorEvent_iMUEvent_tag;
     pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    if (!pb_encode(&stream, TestMessage_fields, &test_m))
+    if (!pb_encode(&stream, IMUEvent_fields, &test))
     {
         Serial.println("failed to encode temp proto");
         Serial.println(PB_GET_ERROR(&stream));
         return;
     }
-
-    Serial.print("sending message...");
-    Serial.println(test_m.test_number);
-    client.write(buffer, stream.bytes_written);
+	
+	webSocket.loop();
+	if (webSocket.isConnected()){
+		Serial.println("sending message...");
+		webSocket.sendBIN(buffer, stream.bytes_written);
+		delay(1000);
+	}
 }
