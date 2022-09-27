18bb587551c7bc43a7a4b701b6c5fc812d9c731c
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2022-09-27 16:01:32 +0300

    Pitot test
    Successful

--- Pitot/Test/Test.ino
+++ Pitot/Test/Test.ino
@@ -1,17 +1,16 @@
 int pressure;
-int Rx = 0;
-int Tx = 1;
+int pressSensor = A0;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(Rx, INPUT);
-pinMode(Tx, OUTPUT);
+pinMode(pressSensor, INPUT);
 Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-pressure = Serial.available();
+pressure = analogRead(pressSensor);
+pressure = map(pressure, 0, 1023, 0, 100000); //100000Pa is an assumed value
 Serial.println(pressure);
 delay(300);
 }
