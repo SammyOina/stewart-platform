f55a78c95044c35d6689459cc8fbe1547023798b
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2022-12-01 16:12:36 +0300

    Pitot tube
    Pitot tube Pa readings | Smoke Connector

---
+++ Pitot/Range.md
@@ -0,0 +1,6 @@
+# Pressure Ranges at various speeds
+11020pa - 11251pa at 30m/s
+
+11509pa - 11904pa at 40m/s
+
+11966pa - 12326pa at 50m/s
--- Pitot/Test/Test.ino
+++ Pitot/Test/Test.ino
@@ -1,16 +1,37 @@
+#include <WiFi.h>
+const char* ssid = "sammy"
+const char* password = "12345678"
+
+
 int pressure;
-int pressSensor = A0;
+int pressSensor = 15;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(pressSensor, INPUT);
-Serial.begin(9600);
+Serial.begin(115200);
+delay(1000);
+WiFi.mode(WIFI_STA);
+WiFi.begin(ssid, password);
+Serial.println("\n Connecting");
+
+while (WiFi.status() != WL_CONNECTED)
+{
+  Serial.print(".")
+  delay(100);
+}
+Serial.println("\nConnected to the WiFi network")
+{
+  Seial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 pressure = analogRead(pressSensor);
-pressure = map(pressure, 0, 1023, 0, 100000); //100000Pa is an assumed value
-Serial.println(pressure);
+pressure = map(pressure, 0, 1023, -2000, 2000); //1kpa == 0.145psi
+Serial.print(pressure);
+Serial.println("pa");
 delay(300);
 }
